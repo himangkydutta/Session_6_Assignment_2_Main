d0f52c2f0c2ff1f31286cf16834e1c8b2f5e7c4d
himangkydutta <himangkydutta08@gmail.com>
2017-04-30 20:03:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//Here we are using if-else comparison and finding the greatest number
+var NumberOne = 20;
+var NumberTwo = 30;
+var NumberThree = 40;
+
+if((NumberOne>NumberTwo) && (NumberOne>NumberThree))
+{
+    console.log("The greatest number is :- "+NumberOne);
+}
+else if((NumberTwo>NumberOne) && (NumberTwo>NumberThree))
+{
+    console.log("The greated number is :- "+NumberTwo);
+}
+else
+{
+    console.log("The greated number is :- "+NumberThree);
+}
+	
